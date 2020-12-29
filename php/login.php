543698b6b37fae286820ee425016277deb5c3b54
Adam Sellin <marcsellin2016@gmail.com>
2020-12-29 16:37:17 +0100

    add setup

--- php/login.php
+++ php/login.php
@@ -1,3 +1,69 @@
 <?php
-phpinfo();
+	//Démarrage de l'environnement de session
+	session_start() ;
+
+	//Connexion au serveur MySQL
+	$servername = "localhost";
+	$username = "root";
+	$password = "root123";
+	$database = "docker_contact";
+    $port = "3306";
+
+	try {
+		$linkpdo = new PDO("mysql:host=$servername;port=$port;dbname=$database", $username, $password);
+	} catch(PDOException $e) {
+		echo "Connection failed: " . $e->getMessage();
+	}
+
+
+	//Initialisation des variables de session si l'utilisateur a validé le formulaire
+	if (isset($_POST['button']) && ($_POST['button'] == 'CONNEXION')) {
+		if (isset($_POST['login']) && !empty($_POST['login']) && isset($_POST['passwd']) && !empty($_POST['passwd'])) {
+			$select = $linkpdo->query("SELECT identifiant, mdp FROM `profils` WHERE `identifiant`= '".$_POST['login']."'") ;
+				$data = $select->fetch() ;
+				$identifiant = $data['identifiant'] ;
+				$passwd = $data['mdp'] ;
+			if ($_POST['login'] == $identifiant && $_POST['passwd'] == $passwd) {
+				$_SESSION['access'] = 'oui' ;
+				$_SESSION['identifiant'] = $identifiant ;
+				header("Location:index.php") ;
+			} else {
+				$_SESSION['access'] = 'retente' ;
+			}
+		}
+	}
+
 ?>
+
+<!DOCTYPE HTML>
+<html lang="fr-FR">
+   <head>
+   		<title>Login</title>
+		<meta charset="utf-8"/>
+		<link rel="stylesheet" href="style.css">
+   </head>
+	<body>
+
+		<form class="login" method="post" action="login.php">
+			<div class="centered">
+				<div class="blank"></div>
+
+				<div class="blank"></div>
+				<h3>IDENTIFIANT</h3>
+				<input class="ilogin" type="text" name="login" />
+				<div class="blank"></div>
+				<h3>MOT DE PASSE</h3>
+				<input class="ilogin" type="password" name="passwd" /> <br /> <br />
+				<a href="mdpoublie.php">Mot de passe oublié ?</a> <br />
+				<a href="inscription.php">Pas de compte ? Inscrivez-vous.</a> <br />
+				<div class="blank"></div>
+				<div class="formb">
+				<input type="submit" value="CONNEXION" name="button" />
+				</div>
+				<div class="blank"></div>
+			</div>
+		</form>
+
+	</body>
+</html>
+
---
+++ setup.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `contacts` ( `id` int(11) NOT NULL AUTO_INCREMENT, `nom` varchar(50) NOT NULL, `prenom` varchar(50) NOT NULL, `email` varchar(50) NOT NULL, `date_naissance` date NOT NULL, `telephone` varchar(10) NOT NULL, `idParent` varchar(50) NOT NULL, PRIMARY KEY (`id`), KEY `idParent` (`idParent`) ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8
+
+INSERT INTO `contacts` (`id`, `nom`, `prenom`, `email`, `date_naissance`, `telephone`, `idParent`) VALUES (3, 'Russeil', 'Margaux', 'margaux.russeil.19@gmail.com', '1999-09-19', '0652414243', 'Margaux'), (4, 'Russeil', 'Sylvie', 'Fasm.russeil@free.fr', '1969-01-06', '0680513411', 'Sylvie')
+
+CREATE TABLE IF NOT EXISTS `profils` ( `identifiant` varchar(50) NOT NULL, `mdp` varchar(50) NOT NULL, `nom` varchar(50) NOT NULL, `prenom` varchar(50) NOT NULL, `date_naissance` date NOT NULL, `email` varchar(50) NOT NULL, `telephone` varchar(10) NOT NULL, PRIMARY KEY (`identifiant`), KEY `identifiant` (`identifiant`) ) ENGINE=InnoDB DEFAULT CHARSET=utf8
+
+INSERT INTO `profils` (`identifiant`, `mdp`, `nom`, `prenom`, `date_naissance`, `email`, `telephone`) VALUES ('Margaux', 'azerty', 'Russeil', 'Margaux', '1999-09-19', 'margaux.russeil.19@gmail.com', '0652414243'), ('Sylvie', 'azerty', 'Russeil', 'Sylvie', '1969-01-06', 'Fasm.russeil@free.fr', '0680513411')
